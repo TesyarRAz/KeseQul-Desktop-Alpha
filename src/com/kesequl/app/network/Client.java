8eb64e0960f377c2a6f495e2a2dd645c3bd5f3f1
faisal-t <54567065+faisal-t@users.noreply.github.com>
2019-11-12 18:58:20 +0700

    Update Client.java

--- src/com/kesequl/app/network/Client.java
+++ src/com/kesequl/app/network/Client.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -33,7 +33,7 @@ import org.apache.http.message.BasicNameValuePair;
  * @author user65
  */
 public class Client {
-//    public static final String BASE_HOST = "https://kesequl.exposei.com/";
+     public static final String BASE_HOST = "https://kesequl.exposei.com/";
      public static final String BASE_HOST = "http://localhost/kesequl/";
     public static final String HOST_API = BASE_HOST + "Api/";
     public static final String HOST_ASSET = BASE_HOST + "assets/upload/";
